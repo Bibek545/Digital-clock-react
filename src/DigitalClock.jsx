fd31a964e5ec6bdf2f41fa58e3cb81f7d6ee9fb7
Bibek Hanal <bbekhamal26@gmail.com>
2025-06-10 18:08:18 +1000

    added js functions and works fine

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -2,11 +2,36 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 
 const DigitalClock = () => {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(()=> {
+       const intervalId = setInterval(()=> {
+             setTime(new Date());
+       }, 1000);
+
+       return () => {
+        clearInterval(intervalId);
+       }
+    }, []);
     
+    const formatTime = () => {
+        let hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        const meridiem = hours >= 12 ? "PM" : "AM";
+
+        hours = hours % 12 || 12;
+
+        return `${padZero(hours)} : ${padZero(minutes)} : ${padZero(seconds)} ${meridiem}`;
+    }
+
+    const padZero = (number) => {
+         return (number < 10 ? "0" : "") + number;
+    }
   return (
     <div className='clock-container'>
       <div className='clock'>
-        <span><h1>00:00:00</h1></span>
+        <span><h1>{formatTime()}</h1></span>
         </div>  
       
     </div>
