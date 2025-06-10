f250c6f51e16e26744454b8d80d295808df1ce2b
Bibek Hanal <bbekhamal26@gmail.com>
2025-06-10 17:53:58 +1000

    finished setting up the project and applied css as well

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import DigitalClock from "./DigitalClock";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>My digital clock.</h1>
+      <DigitalClock />
+    </>
+  );
+}
+
+export default App;
---
+++ src/DigitalClock.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+const DigitalClock = () => {
+    
+  return (
+    <div className='clock-container'>
+      <div className='clock'>
+        <span><h1>00:00:00</h1></span>
+        </div>  
+      
+    </div>
+  )
+}
+
+export default DigitalClock;
